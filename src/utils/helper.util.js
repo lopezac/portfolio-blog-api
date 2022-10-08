3d3a03217e9b7e2592f2ae2e99dae5f3e96e8f47
Axel C. Lopez <lopezaxel@protonmail.com>
2022-10-08 17:08:06 -0400

    finish sort and filter

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -17,17 +17,18 @@ function getFilterQuery(query) {
   return filterOptions;
 }
 
-function getSortQuery(query) {
+function getSortQuery(query = "+timestamp") {
   const sortOptions = {};
   const sortQuery = query.split(",");
   for (const option of sortQuery) {
     // There is a bug, '+' character appears as ' ' for some reason.
     // Later study it and become article idea.
+    // 1 means ascendent and -1 descendent
     const sortOrder = option[0] === " " ? 1 : -1;
     const sortName = option.slice(1);
     sortOptions[sortName] = sortOrder;
   }
-  console.log("sortOptions", sortOptions);
+  console.log("query", sortOptions);
   return sortOptions;
 }
 
