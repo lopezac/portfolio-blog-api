e303216ebe357063196356466c90db6905145586
Axel Carlos Lopez <lopezaxel@protonmail.com>
2022-10-14 12:01:35 -0300

    Small fixs

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
   post: { type: Schema.Types.ObjectId, required: true },
   text: { type: String, required: true, minLength: 2, maxLength: 500 },
-  username: { type: String, required: true, minLength: 2, maxLength: 125 },
+  username: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 125,
+    default: "Anonymous",
+  },
   timestamp: { type: Date, default: new Date() },
 });
 
