7bc47e98405954d0b818fc88b02ec3af2906534d
Axel Carlos Lopez <lopezaxel@protonmail.com>
2022-10-18 10:07:17 -0300

    Fix db production error

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -4,6 +4,7 @@ const { NODE_ENV, DB_URL_DEVELOPMENT, DB_URL_PRODUCTION } = process.env;
 console.log("node_env", NODE_ENV);
 const mongoDB =
   NODE_ENV === "development" ? DB_URL_DEVELOPMENT : DB_URL_PRODUCTION;
+console.log("mongoDB", mongoDB);
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
